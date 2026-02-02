700789d26962605ec0c686996bd3faa793f8ceff
andooo26 <123529443+andooo26@users.noreply.github.com>
2026-02-02 22:24:11 +0900

    ログイン画面の修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body>
+    <html lang="ja" style={{ margin: 0, padding: 0, height: '100%', background: '#808080' }}>
+      <body style={{ margin: 0, padding: 0, height: '100%', background: '#808080' }}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
